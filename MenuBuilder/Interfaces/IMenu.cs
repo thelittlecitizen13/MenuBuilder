729a938c1fb34a6992525068120dcb40e38dd855
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-08-18 13:59:15 +0300

    IMenu interface addeD

---
+++ MenuBuilder/Interfaces/IMenu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace MenuBuilder
+{
+    interface IMenu
+    {
+        public StringBuilder MenuDisplay { get; set; }
+        public string MenuName { get;}
+        public string MainOutput { get; }
+
+        void ShowMenu();
+    }
+}
---
+++ MenuBuilder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MenuBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
