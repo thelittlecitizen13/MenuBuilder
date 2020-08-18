514e18dbc9c3563446c59aba343d2985bf3e3645
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-08-18 17:15:10 +0300

    empty IMenu interface added

--- MenuBuilder/Actions/PrintNumbersAction.cs
+++ MenuBuilder/Actions/PrintNumbersAction.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace MenuBuilder.Actions
+namespace MenuBuilder
 {
     class PrintNumbersAction : IAction
     {
--- MenuBuilder/Actions/ShowNextMenuAction.cs
+++ MenuBuilder/Actions/ShowNextMenuAction.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace MenuBuilder.Actions
+namespace MenuBuilder
 {
     class ShowNextMenuAction : IAction
     {
