ec46f7071ce2c56aa2613550502ac6e0699252d3
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-08-18 14:22:29 +0300

    StringMenu class implemented

--- MenuBuilder/Classes/StringMenu.cs
+++ MenuBuilder/Classes/StringMenu.cs
@@ -4,7 +4,42 @@ using System.Text;
 
 namespace MenuBuilder.Classes
 {
-    class StringMenu
+    class StringMenu : IMenu
     {
+        public Dictionary<string, IAction> ActionOptions { get; set; }
+        public StringBuilder MenuDisplay { get; set; }
+
+        public string MenuName { get; set; }
+
+        public string MainOutput { get; set; }
+
+        public void Run()
+        {
+            if (!IsMenuBuilt())
+            {
+                Console.WriteLine($"Menu ${MenuName} - No menu to show");
+                return;
+            }
+            ShowMenu();
+        }
+
+        public void ShowMenu()
+        {
+            Console.WriteLine(MenuDisplay.ToString());
+        }
+
+
+        public StringMenu(Dictionary<string,IAction> actions, string name, string mainOutput)
+        {
+            ActionOptions = actions;
+            MenuDisplay = new StringBuilder();
+            MenuName = name;
+            MainOutput = mainOutput;
+        }
+
+        private bool IsMenuBuilt()
+        {
+            return ! string.IsNullOrEmpty(Convert.ToString(MenuDisplay));
+        }
     }
 }
