acaa497b21a8651a6171efb46e7be9a3a715b61e
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-08-18 16:36:53 +0300

    Input Handler class added

---
+++ MenuBuilder/Classes/InputHandler.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MenuBuilder.Classes
+{
+    class InputHandler<T> where T : IConvertible
+    {
+        public int ReadIntParser()
+        {
+            int choice = 0;
+            while (choice == 0)
+            {
+                try
+                {
+                    choice = int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Wrong input, please try again");
+                }
+            }
+            return choice;
+        }
+
+        public bool IsChoiceValid(Menu<T> menuObject, T choice)
+        {
+            return menuObject.ActionOptions.ContainsKey(choice);
+        }
+
+        public List<string> GetStringInputs (int numberOfInputs, string output)
+        {
+            Console.WriteLine(output);
+            List<string> inputs = new List<string>();
+            int countOfGoodInputs = 0;
+            while (countOfGoodInputs != numberOfInputs)
+            {
+                string input = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(input))
+                    inputs.Add(input);
+                else
+                    Console.WriteLine("Wrong input");
+            }
+            return inputs;
+        }
+        public List<int> GetNumericInputs(int numberOfInputs, string output)
+        {
+            Console.WriteLine(output);
+            List<int> inputs = new List<int>();
+            for (int i = 0; i < numberOfInputs; i++)
+            {
+                int input = ReadIntParser();
+                inputs.Add(input);
+            }
+            return inputs;
+        }
+    }
+}
